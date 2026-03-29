8e7f552fb520e8c14415dfeb96191f11bee40077
Maryam <98962702+MaryamAli-2020@users.noreply.github.com>
2026-03-29 11:36:52 +0400

    Add explicit Vercel API route entries

---
+++ api-handler.ts
@@ -0,0 +1,12 @@
+let appPromise:
+  | Promise<ReturnType<(typeof import("./backend/src/app.js"))["createApp"]>>
+  | undefined;
+
+export default async function handler(request: unknown, response: unknown) {
+  if (!appPromise) {
+    appPromise = import("./backend/src/app.js").then(({ createApp }) => createApp());
+  }
+
+  const app = await appPromise;
+  return app(request, response);
+}
